fcc7b6893e3b02038e362a2e583159595a561b46
manjot298 <manjotsingh28734@gmail.com>
2026-04-15 12:08:59 -0400

    Update questions.js

--- questions.js
+++ questions.js
@@ -4,4 +4,5 @@ Who created Manjot OS?|Bill Gates|Manjot Singh|Steve Jobs|1
 Is Python a snake or a language?|Snake|Language|Both|2
 Who is the PM of INDIA?|Modi|MAN|Bill Gates|0
 Who created Windows-11?|Bill Gates|Manjot Singh|Steve Jobs|0
+Which has more weight?|1kg - Feather|1kg - Iron|Both|2
 `.trim();
