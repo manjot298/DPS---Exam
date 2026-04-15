11f3e1b7364b09dd7f47903e1cb8ea73d738db36
manjot298 <manjotsingh28734@gmail.com>
2026-04-15 12:04:10 -0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,7 @@
+const quizSource = `
+What is 10 + 20?|30|40|50|0
+Who created Manjot OS?|Bill Gates|Manjot Singh|Steve Jobs|1
+Is Python a snake or a language?|Snake|Language|Both|2
+Who is the PM of INDIA?|Modi|MAN|Bill Gates|0
+Who created Windows-11?|Bill Gates|Manjot Singh|Steve Jobs|0
+`.trim();
