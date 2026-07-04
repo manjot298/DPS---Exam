c70994197a531b2a74b8ec497a590b40e4097bf6
manjot298 <manjotsingh28734@gmail.com>
2026-07-04 21:29:48 +0530

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,8 +1,8 @@
 const quizSource = `
-What is 10 + 20?|30|40|50|0
-Who created Manjot OS?|Bill Gates|Manjot Singh|Steve Jobs|1
-Is Python a snake or a language?|Snake|Language|Both|2
-Who is the PM of INDIA?|Modi|MAN|Bill Gates|0
-Who created Windows-11?|Bill Gates|Manjot Singh|Steve Jobs|0
-Which has more weight?|1kg - Feather|1kg - Iron|Both|2
+Which of the following factors increases the rate of evaporation of a liquid?|Decrease in surface area|Increase in humidity|Increase in temperature|2
+An object moves in a circular path of radius r. What is its displacement after completing half a revolution?|Zero|π r|2r|2
+Which cell organelle is actively involved in synthesizing proteins within a cell?|Lysosom|Ribosome|Vacuole|1
+If the distance between two masses is doubled, the gravitational force between them becomes:|One-fourth|Half|Doubled|0
+The chemical formula of Magnesium Chloride is:|MgCl|MgCl₂|Mg₂Cl|1
+The point at which the X-axis and Y-axis intersect in a Cartesian plane is called:|Abscissa|Ordinate|Origin|2
 `.trim();
